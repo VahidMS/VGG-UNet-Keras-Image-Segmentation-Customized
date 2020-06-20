48caffcb952d16c161926348de127607187eb675
Vahid M. Safarzadeh <vahid.msafarzadeh@gmail.com>
2020-06-20 19:16:18 +0430

    Create unet.py
    Only functions _unet and vgg_unet() from the original version.
    A trans_conv is added after the last convolution to upsample the output. (currently only to 64x64 version)

---
+++ unet.py
@@ -0,0 +1,61 @@
+#@title _unet and vgg_net {form-width: '10%'}
+#original version from: keras_segmentation/models/unet.py
+
+if IMAGE_ORDERING == 'channels_first':
+    MERGE_AXIS = 1
+elif IMAGE_ORDERING == 'channels_last':
+    MERGE_AXIS = -1
+def _unet(n_classes, encoder, l1_skip_conn=True, input_height=416,
+          input_width=608):
+
+    img_input, levels = encoder(
+        input_height=input_height, input_width=input_width)
+    [f1, f2, f3, f4, f5] = levels
+
+    o = f4
+
+    o = (ZeroPadding2D((1, 1), data_format=IMAGE_ORDERING))(o)
+    o = (Conv2D(512, (3, 3), padding='valid' , activation='relu' , data_format=IMAGE_ORDERING))(o)
+    o = (BatchNormalization())(o)
+
+    o = (UpSampling2D((2, 2), data_format=IMAGE_ORDERING))(o)
+    o = (concatenate([o, f3], axis=MERGE_AXIS))
+    o = (ZeroPadding2D((1, 1), data_format=IMAGE_ORDERING))(o)
+    o = (Conv2D(256, (3, 3), padding='valid', activation='relu' , data_format=IMAGE_ORDERING))(o)
+    o = (BatchNormalization())(o)
+
+    o = (UpSampling2D((2, 2), data_format=IMAGE_ORDERING))(o)
+    o = (concatenate([o, f2], axis=MERGE_AXIS))
+    o = (ZeroPadding2D((1, 1), data_format=IMAGE_ORDERING))(o)
+    o = (Conv2D(128, (3, 3), padding='valid' , activation='relu' , data_format=IMAGE_ORDERING))(o)
+    o = (BatchNormalization())(o)
+
+    o = (UpSampling2D((2, 2), data_format=IMAGE_ORDERING))(o)
+
+    if l1_skip_conn:
+        o = (concatenate([o, f1], axis=MERGE_AXIS))
+
+    o = (ZeroPadding2D((1, 1), data_format=IMAGE_ORDERING))(o)
+    o = (Conv2D(64, (3, 3), padding='valid', activation='relu', data_format=IMAGE_ORDERING))(o)
+    o = (BatchNormalization())(o)
+
+    o = Conv2D(n_classes, (3, 3), padding='same', data_format=IMAGE_ORDERING)(o)
+
+    # ups_layer = keras.layers.UpSampling2D(size=(2, 2), name="Vahid_Ups", data_format=IMAGE_ORDERING)
+    # o = ups_layer(o)
+    trans_conv = Conv2DTranspose(n_classes, kernel_size=(2,2), strides=(2,2), padding='same', data_format=IMAGE_ORDERING)
+    o = trans_conv(o)
+    model = get_segmentation_model(img_input, o)
+
+    return model
+
+def vgg_unet(n_classes, input_height=416, input_width=608, encoder_level=3):
+
+    model = _unet(n_classes, get_vgg_encoder,
+                  input_height=input_height, input_width=input_width)
+    model.model_name = "vgg_unet"
+    return model
+
+model = vgg_unet(2, input_height=64, input_width=64)
+model.summary()
+print(model.output_height)
